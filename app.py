4cb48057817c0c21a5fbaaa16a1697fe4cb96972
pranjallllllll <kpranjalllll744@gmail.com>
2025-08-15 23:33:41 +0530

    app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_recommendations(title, cosine_sim=cosine_sim):
 
 # Fetch movie poster from TMDB API
 def fetch_poster(movie_id):
-    api_key = '7b995d3c6fd91a2284b4ad8cb390c7b8'  # Replace with your TMDB API key
+    api_key = '2aa387840c2b9c8e525a08b63027343d'
     url = f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}'
     response = requests.get(url)
     data = response.json()
@@ -46,3 +46,4 @@ if st.button('Recommend'):
                 with col:
                     st.image(poster_url, width=130)
                     st.write(movie_title)
+
